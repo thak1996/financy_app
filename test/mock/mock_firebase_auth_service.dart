294fb72a2232a02d99757dd0b66786cdc91da988
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-08-06 21:29:06 -0300

    Add mock for AuthFirebaseService and implement unit tests for login functionality

---
+++ test/mock/mock_firebase_auth_service.dart
@@ -0,0 +1,4 @@
+import 'package:financy_app/app/core/services/auth_firebase.service.dart';
+import 'package:mockito/mockito.dart';
+
+class MockFirebaseAuthService extends Mock implements AuthFirebaseService {}
---
+++ test/services/auth_service.dart
@@ -0,0 +1,50 @@
+import 'package:financy_app/app/core/exception/auth.exception.dart';
+import 'package:financy_app/app/core/models/user.model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:result_dart/result_dart.dart';
+import '../mock/mock_firebase_auth_service.dart';
+
+void main() {
+  late MockFirebaseAuthService mockFirebaseAuthService;
+
+  setUp(() {
+    mockFirebaseAuthService = MockFirebaseAuthService();
+  });
+
+  group('FirebaseAuthService Tests', () {
+    test('Sign In - Success', () async {
+      when(
+        mockFirebaseAuthService.login(
+          email: 'test@example.com',
+          password: 'password',
+        ),
+      ).thenAnswer(
+        (_) async =>
+            Success(UserModel(email: 'test@example.com', token: 'user_token')),
+      );
+
+      final result = await mockFirebaseAuthService.login(
+        email: 'test@example.com',
+        password: 'password',
+      );
+      expect(result.isSuccess(), true);
+    });
+
+    test('Sign In - Failure', () async {
+      when(
+        mockFirebaseAuthService.login(
+          email: 'invalid@example.com',
+          password: 'wrongpassword',
+        ),
+      ).thenAnswer((_) async => Failure(AuthException('Invalid credentials')));
+
+      final result = await mockFirebaseAuthService.login(
+        email: 'invalid@example.com',
+        password: 'wrongpassword',
+      );
+
+      expect(result.isError(), true);
+    });
+  });
+}

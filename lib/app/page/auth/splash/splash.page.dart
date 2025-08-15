e912fd4ee0ab7e1bbe245b98501664c903136874
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-08-15 09:48:06 -0300

    refactor: simplify SplashPage structure by removing unnecessary BlocBuilder and improving listener logic

--- lib/app/page/auth/splash/splash.page.dart
+++ lib/app/page/auth/splash/splash.page.dart
@@ -20,43 +20,43 @@ class _SplashPageState extends State<SplashPage> {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => SplashController(SecureStorage())..checkAuth(),
-      child: BlocBuilder<SplashController, SplashState>(
-        builder: (context, state) {
-          return BlocListener<SplashController, SplashState>(
-            listener: (context, state) {
-              if (state is SplashSuccess) context.goNamed('app-scaffold');
-              if (state is SplashFailed) context.goNamed('login');
-            },
-            child: Scaffold(
-              body: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: AppColors.gradientBackground,
-                  ),
-                ),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Financy",
-                        style: AppTextStyles.text50(
-                          context,
-                          color: AppColors.white,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      SizedBox(height: 32.h),
-                      CircularProgressIndicator(color: Colors.white),
-                    ],
-                  ),
-                ),
+      child: BlocListener<SplashController, SplashState>(
+        listener: (context, state) {
+          if (!context.mounted) return;
+          if (state is SplashSuccess) {
+            context.goNamed('app-scaffold');
+          } else if (state is SplashFailed) {
+            context.goNamed('login');
+          }
+        },
+        child: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: AppColors.gradientBackground,
               ),
             ),
-          );
-        },
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Financy",
+                    style: AppTextStyles.text50(
+                      context,
+                      color: AppColors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 32.h),
+                  const CircularProgressIndicator(color: Colors.white),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }

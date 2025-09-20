2318e47a0b1d27924771078f612b246e28bec7c7
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-09-20 20:34:48 -0300

    refactor: format AddTransactionError class for improved readability

--- lib/app/page/screens/add_transaction/add_transaction.state.dart
+++ lib/app/page/screens/add_transaction/add_transaction.state.dart
@@ -8,5 +8,6 @@ class AddTransactionSuccess extends AddTransactionState {}
 
 class AddTransactionError extends AddTransactionState {
   final String message;
+  
   AddTransactionError({required this.message});
 }
